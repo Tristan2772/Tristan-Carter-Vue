7d0ca33d16acc43c3cd47fa9df84b520be6d1510
Tristan <tristancarter2772@gmail.com>
2026-01-20 23:46:36 -0600

    small animation bug

--- src/components/AnimationContainer.vue
+++ src/components/AnimationContainer.vue
@@ -1,16 +1,14 @@
 <script setup>
 import GlitchyTheme from './animationComponents/GlitchyTheme.vue'
-import { inject, ref } from 'vue'
+import { inject } from 'vue'
 
 const { animationClass } = inject('animationClass')
-
-const reducedMotion = ref(window.matchMedia('(prefers-reduced-motion: reduce)'))
 </script>
 
 <!-- change animation-container class on button click in header, under themes tab -->
 
 <template>
-  <div v-show="reducedMotion.value" id="animation-container">
+  <div id="animation-container">
     <GlitchyTheme v-if="animationClass === 'glitch'" />
   </div>
 </template>
