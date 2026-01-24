587eb97cf6a412e01b067c9ca5b03bac1671ae53
Tristan Carter <tristancarter2772@gmail.com>
2026-01-24 14:27:36 -0600

    Add files via upload

---
+++ assets/assets/BlogPost1-n3He6Enw.js
@@ -0,0 +1,26 @@
+const n=`---
+title: The Traveling Salesman Problem
+date: 2025-09-25
+---
+
+# The Traveling Salesman Problem
+
+## What is the TSP?
+
+short summary about the TSP and some links to some references. maybe import superscript for footnotes. 4-5 sentence summary and add table of contents possibly
+
+## There's math involved?
+
+## How long has this problem Plagued us?
+
+## What makes it Impossible?
+
+## Why is this Important?
+
+- It is important because the TSP can show up in many different forms.
+- It is an impossible task that industry leaders are still actively working on solving this problem.
+
+## My Thoughts
+
+- Amazon experience running routes
+`;export{n as default};
